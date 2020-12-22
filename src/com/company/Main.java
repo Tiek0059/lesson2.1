199635212ae0285e9b0da0185e9fb126992ef1bc
Tilek0059 <usubalievtilek06@gmail.com>
2020-12-22 18:26:17 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Main {
+
+    public static double main(String[] args) {
+        int temperature = 5;
+        int age = 18;
+        double result = temperature + age;
+        return result;
+        }
+    }
+
