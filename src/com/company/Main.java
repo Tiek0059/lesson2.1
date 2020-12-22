4f0830e9b21a6fbaea57fe2a5018915a089fb7d6
Tilek0059 <usubalievtilek06@gmail.com>
2020-12-22 18:46:51 +0600

    hw2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,23 @@ package com.company;
 
 public class Main {
 
-    public static double main(String[] args) {
-        int temperature = 5;
-        int age = 18;
-        double result = temperature + age;
-        return result;
-        }
+    public static void main(String[] args) {
+        System.out.println(isGoOutside(0, 6));
+        System.out.println(isGoOutside(6, 0));
+        System.out.println(isGoOutside(6460, 57));
+        System.out.println(isGoOutside(77, 6));
+        System.out.println(isGoOutside(0, 8));
     }
 
+    public static String isGoOutside(int age, int temp) {
+        if (age > 20 && age < 45 && temp > -20 && temp < 30)
+            return "Можно идти гулять";
+        else if (age < 20 && temp > 0 && temp < 28)
+            return "Можно идти гулять";
+        else if (age > 45 && temp > -10 && temp < 25)
+            return "Можно идти гулять";
+        else
+            return "Оставайтесь дома";
+    }
+
+}
