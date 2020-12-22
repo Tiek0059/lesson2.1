80bae682d7d176de98fa3f855fcd8d5f4578966c
Tilek0059 <usubalievtilek06@gmail.com>
2020-12-22 20:15:52 +0600

    hw2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,29 @@
 package com.company;
 
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isGoOutside(0, 6));
-        System.out.println(isGoOutside(6, 0));
-        System.out.println(isGoOutside(6460, 57));
-        System.out.println(isGoOutside(77, 6));
-        System.out.println(isGoOutside(0, 8));
+        System.out.println(isGoOutside(7,10));
+        System.out.println(isGoOutside(55,-10));
+        System.out.println(isGoOutside(60,0));
+        System.out.println(isGoOutside(9,20));
+        System.out.println(isGoOutside(34,17));
     }
 
     public static String isGoOutside(int age, int temp) {
-        if (age > 20 && age < 45 && temp > -20 && temp < 30)
+        if (age > 20 && age < 45 && temp > -20 && temp < 30) {
             return "Можно идти гулять";
-        else if (age < 20 && temp > 0 && temp < 28)
+        } else if (age < 20 && temp > 0 && temp < 28) {
             return "Можно идти гулять";
-        else if (age > 45 && temp > -10 && temp < 25)
+        }else if(age > 45 && temp > -10 && temp < 25){
             return "Можно идти гулять";
-        else
-            return "Оставайтесь дома";
+        }else {
+            return "Оставайся дома";
+        }
+
     }
 
 }
